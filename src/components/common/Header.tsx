84362654199a526eb1e52b6956cf252f8a1ef103
koharusasafu <koharusasafu@gmail.com>
2024-09-17 14:48:16 +0900

    loginボタン&login機能のみ実装

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,10 +5,21 @@ export const Header = ({ title }: { title: string }) => {
   const context = AuthContextConsumer();
   return (
     <header className="bg-gray-800 bg-opacity-50 backdrop-blur-md shadow-lg border-b border-gray-700">
-      <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-2xl font-semibold text-gray-100">{title}</h1>
+      <div className="flex justify-between items-center max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8">
+        <h1 className=" text-2xl font-semibold text-gray-100">{title}</h1>
+        <button
+          onClick={context.login}
+          className="
+            box-border inline-block h-11 transform-gpu
+            cursor-pointer touch-manipulation whitespace-nowrap rounded-lg border-b-4 border-solid border-transparent
+            bg-sky-600 px-4 py-3 text-center text-sm font-bold uppercase leading-5 tracking-wider text-white outline-none
+            transition-all duration-200 hover:brightness-110
+            active:border-b-0 active:border-t-4 active:bg-none disabled:cursor-auto"
+        >
+          login
+          <span className="absolute inset-0 -z-10 rounded-lg border-b-4 border-solid border-transparent bg-sky-500" />
+        </button>
       </div>
-      <button onClick={context.login}>login</button>
     </header>
   );
 };
